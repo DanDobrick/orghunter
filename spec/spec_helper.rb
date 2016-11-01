b09de60c667b6b4ecaf602a3af843e3322b44664
Dan Dobrick <dandobrick@gmail.com>
2016-11-01 14:23:13 -0500

    Reset API key and remove it from the file because I make a HUGE mistake

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,19 +4,16 @@ require "vcr"
 require "httparty"
 
 
-ORGHUNTER_API_KEY = '3de10e9f24f7ca4c69b9e327ee6a4e68'
-
 VCR.configure do |config|
   config.cassette_library_dir = "../test/fixtures/cassettes"
   config.hook_into :webmock # or :fakeweb
   config.default_cassette_options = { record: :once }   
 end
 
-# class VCRTest < Test::Unit::TestCase
-#   def test_example_dot_com
-#     VCR.use_cassette("synopsis") do
-#       response = Net::HTTP.get_response(URI('http://www.iana.org/domains/reserved'))
-#       assert_match /Example domains/, response.body
-#     end
-#   end
-# end
+RSpec.configure do |config|
+  config.before(:all) do 
+    Orghunter.configure do |config|
+      config.api_key = ENV['ORGHUNTER_API_KEY']
+    end
+  end  
+end
