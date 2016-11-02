66866aa8ab0fb089c8f369e08ff14784f6b1c57b
Dan Dobrick <dandobrick@gmail.com>
2016-11-02 14:26:49 -0500

    Build query string when creating new search

--- lib/search.rb
+++ lib/search.rb
@@ -5,23 +5,20 @@ module Orghunter
     def initialize(args)
 #     I'm sure there's a better way than all of these ifs. I need to figure out the behavior with ||= or || or
 # Ternary is the same, but I've never used it so maybe I will
-      @search_term = "&searchTerm=#{args[:search_term]}" if args [:search_term]
-      @city = "&city=#{args[:city]}" if args[:city]
-      @state = "&state=#{args[:state]}"
-      @zip_code = "&zipCode=#{args[:zip_code]}"
-      @category = "&category=#{args[:category]}"
-      if args[:eligible] == true
-        @eligible = "&eligible=1"
-      else
-        @eligible = ''
-      end
-      @rows = "&rows=#{args[:rows]}"
-      @start = "&start=#{args[:start]}"
+      @ein          = args[:ein]          ? "&ein=#{args[:ein]}"                  : 
+      @search_term  = args[:search_term] ? "&searchTerm=#{args[:search_term]}"    : ''
+      @city         = args[:city]         ? "&city=#{args[:city]}"                : ''
+      @state        = args[:state]        ? "&state=#{args[:state]}"              : ''
+      @zip_code     = args[:zip_code]     ? "&zipCode=#{args[:zip_code]}"         : ''
+      @category     = args[:category]     ? "&category=#{args[:category]}"        : ''
+      @eligible     = args[:eligible]     ? "&eligible=1"                         : ''
+      @rows         = args[:rows]         ? "&rows=#{args[:rows]}"                : ''
+      @start        = args[:start]        ? "&start=#{args[:start]}"              : ''
       query_api
     end
 
     def create_string
-      @search_string = "http://data.orghunter.com/v1/charitysearch?user_key=#{Orghunter.configuration.api_key}#{@search_term}#{@city}#{@state}#{@zip_code}#{@category}#{@eligible}#{@rows}#{@start}"
+      @search_string = "http://data.orghunter.com/v1/charitysearch?user_key=#{Orghunter.configuration.api_key}#{@ein}#{@search_term}#{@city}#{@state}#{@zip_code}#{@category}#{@eligible}#{@rows}#{@start}"
     end
 
     def query_api
