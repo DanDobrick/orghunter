7ecdf8ce0b4983f3812134b98c86f3b040eb8880
Dan Dobrick <dandobrick@gmail.com>
2016-11-02 12:15:34 -0500

    Add spec for other kinds of searches

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,50 +1,66 @@
 require "spec_helper"
 
 describe Orghunter::Search do
-  let(:args){
-    {
-     search_term: 'cows',
-     rows:    22,
-     start:   1
-    }
-  }
-  # user_key:     string    required    Your API Key
-  # ein:          number    optional    Employer Identification Number (EIN)
-  # searchTerm:   string    optional    Charity Name or Keyword. Example: "humane society" or "cancer"
-  # city:         string    optional    city name. Example: "Miami"
-  # state:        string    optional    state name - Two letter state abbreviation
-  # zipCode:      string    optional    zipcode value - 5 digit zipcode value
-  # category:     string    optional    Category Value Selected from Categories API
-  # eligible:     string    optional    eligible=1 will return only organizations that are tax deductible and in good standing with the IRS
-  # rows:         number    optional    Records Per Page. Default Value = 20
-  # start:        number    optional    Record Set Start Position
+  let(:term_search){ Orghunter::Search.new( {search_term: 'cows',rows: 22, start: 1} ) }
+  let(:term_search_results){term_search.results}
+  let(:ein_search){ Orghunter::Search.new({ein: 261688229}) }
+  let(:city_search){ Orghunter::Search.new({city: 'Chicago'}) }
+  let(:state_search){ Orghunter::Search.new({city: 'IL'}) }
+  let(:zip_code_search){ Orghunter::Search.new({city: '60606'}) }
+  # let(:category_search){ Orghunter::Search.new({city: 'Chicago'}) }
+  let(:eligible_search){ Orghunter::Search.new({eligible: true}) }
 
-  let(:search){Orghunter::Search.new(args)}
-  let(:search_results){search.results}
 
   it 'should exist' do
-    expect(search).to be_kind_of(Orghunter::Search)
+    expect(term_search).to be_kind_of(Orghunter::Search)
   end
 
   it "Has an Array of Charities" do
-    expect(search.results).to be_kind_of(Array)
+    expect(term_search.results).to be_kind_of(Array)
     search.results.each{|charity| expect(charity).to be_kind_of(Orghunter::Charity)}
   end
 
   it "Has a count of the results" do
-    expect(search.count).to eq(22)
+    expect(term_search.count).to eq(22)
   end
 
   it "Has a record start position" do
-    expect(search.start_position).to eq(1)
+    expect(term_search.start_position).to eq(1)
   end
 
   it "Has an ending position" do
-    expect(search.final_position).to eq(23)
+    expect(term_search.final_position).to eq(23)
   end
 
   it "Has a number of rows returned" do
-    expect(search.rows_returned).to eq(22)
+    expect(term_search.rows_returned).to eq(22)
   end
+
+  it "Returns one charity when searching by EIN" do
+    expect(ein_search.results).to have(1).charity
+  end
+
+  it "Returns the charity with a particular EIN when searching by ein" do
+    returned_charity = ein_search.results[0]
+    expect(returned_charity).to be_kind_of(Orghunter::Charity)
+    expect(returned_charity.ein).to eq(261688229)
+  end
+
+  it "returns results with the same city if searching for a city`" do
+    city_search.results.each {|charity| expect(charity.city).to eq('Chicago')}
+  end
+
+  it "returns results with the same state if searching for a state`" do
+    state_search.results.each {|charity| expect(charity.state).to eq('IL')}
+  end 
+
+  it "returns results with the same zip_code if searching for a zip_code`" do
+    zip_code_search.results.each {|charity| expect(charity.zip_code).to eq('Chicago')}
+  end
+
+  it "returns results with the same eligibility if searching for a eligibility`" do
+    elegibility_search.results.each {|charity| expect(charity.elegibile?).to eq(true)}
+  end
+
 
 end
