bab197a94a25a0f936de6cf65d54ea2c9aa4ee9e
Dan Dobrick <dandobrick@gmail.com>
2016-11-08 16:14:44 -0600

    Write spec for all_categories method in Orghunter

--- spec/orghunter_spec.rb
+++ spec/orghunter_spec.rb
@@ -4,4 +4,19 @@ describe Orghunter do
   it "has a version number" do
     expect(Orghunter::VERSION).not_to be nil
   end
+
+  describe 'Orghunter.all_categories' do
+      it 'returns an array' do
+        VCR.use_cassette('categories') do
+          expect(Orghunter.all_categories).to be_kind_of(Array)
+        end
+      end
+
+      it 'each item in the array is a category object' do
+        VCR.use_cassette('categories') do
+          all_categories = Orghunter.all_categories
+          all_categories.each{|category| expect(category).to be_kind_of(Orghunter::Category)}
+        end
+      end
+  end
 end
