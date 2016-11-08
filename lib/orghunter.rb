ed4acaaf56b5414ce79504bbc8618a2d0cd677e4
Dan Dobrick <dandobrick@gmail.com>
2016-11-08 16:15:35 -0600

    Write all_charities method

--- lib/orghunter.rb
+++ lib/orghunter.rb
@@ -22,4 +22,11 @@ module Orghunter
     yield(configuration)
   end
 
+  def self.all_categories
+    query_uri = URI::encode("http://data.orghunter.com/v1/charitysearch?user_key=#{Orghunter.configuration.api_key}")
+    response = Net::HTTP.get_response(URI(query_uri))
+    categories_json = JSON.parse(response.body)['data']
+    results = categories_json.map{|category_hash| Category.new(category_hash)}
+  end
+
 end
