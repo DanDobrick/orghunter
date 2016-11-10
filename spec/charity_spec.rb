0d6841aaf6b4856e8ea8918fae4bb2395de90680
Dan Dobrick <dandobrick@gmail.com>
2016-11-10 10:24:46 -0600

    Realize charity properties have no place in the orghunter spec file

--- spec/charity_spec.rb
+++ spec/charity_spec.rb
@@ -1,6 +1,10 @@
 require "spec_helper"
 
 describe Orghunter::Charity do 
+  Orghunter.configure do |config|
+    config.api_key = ENV['ORGHUNTER_API_KEY']
+  end
+  
   args = {"ein" => "261688229",
     "charityName" => "KIDS COWS AND MORE INC",
     "category" => "Arts, Culture and Humanities",
@@ -80,4 +84,39 @@ describe Orghunter::Charity do
   it "Returns true or false to accepting donations" do
     expect(charity.accepting_donations?).to eq(true)
   end
+
+  describe "When getting basic_info on charity" do
+    VCR.use_cassette('basic_info_with_results') do
+      basic_info = Orghunter.basic_info(261688229)
+      it 'returns more information than the basic results' do
+            expect(basic_info.in_care_of).to eq('MARK HALL')
+            expect(basic_info.street).to eq('415 W 30TH ST')
+            expect(basic_info.country).to eq('USA')
+            expect(basic_info.subsection).to eq('501(c)(3)')
+            expect(basic_info.classification).to eq("Charitable Organization")
+            expect(basic_info.affiliation).to eq("Independent - This code is used if the organization is an independent organization or an independent auxiliary (i.e., not affiliated with a National, Regional, or Geographic grouping of organizations).")
+            expect(basic_info.rulling_date).to eq('October, 2008')
+            expect(basic_info.deductibility).to eq("Contributions are deductible.")
+            expect(basic_info.deductibility_status).to eq('PC')
+            expect(basic_info.foundation).to eq("Organization which receives a substantial part of its support from a governmental unit or the general public   170(b)(1)(A)(vi)")
+            expect(basic_info.organization).to eq("Corporation")
+            expect(basic_info.exempt_status).to eq("Unconditional Exemption")
+            expect(basic_info.tax_period).to eq("December, 2014")
+            expect(basic_info.asset_code_desc).to eq("500,000 to 999,999")
+            expect(basic_info.income_code_desc).to eq("10,000 to 24,999")
+            expect(basic_info.filing_requirement_code_desc).to eq("Not required to file (income less than $25,000)")
+            expect(basic_info.pf_filing_requirement).to eq("No PF return")
+            expect(basic_info.accounting_period).to eq("December")
+            expect(basic_info.asset_amount).to eq("570877.00")
+            expect(basic_info.income_amount).to eq("10874.00")
+            expect(basic_info.form990).to eq("10874.00")
+            expect(basic_info.ntee_cd).to eq('A52')
+            expect(basic_info.ntee_class).to eq("Children's Museums")
+            expect(basic_info.ntee_type).to eq("Arts, Culture and Humanities")
+            expect(basic_info.accepting_donation).to eq(true)
+          end
+        end
+  end
+
+
 end
--- spec/orghunter_spec.rb
+++ spec/orghunter_spec.rb
@@ -41,34 +41,6 @@ describe Orghunter do
           expect(charity_info.name).to eq("KIDS COWS AND MORE INC")
         end
 
-        it 'returns more information than the basic results' do
-          expect(charity_info.in_care_of).to eq('MARK HALL')
-          expect(charity_info.street).to eq('415 W 30TH ST')
-          expect(charity_info.country).to eq('USA')
-          expect(charity_info.subsection).to eq('501(c)(3)')
-          expect(charity_info.classification).to eq("Charitable Organization")
-          expect(charity_info.affiliation).to eq("Independent - This code is used if the organization is an independent organization or an independent auxiliary (i.e., not affiliated with a National, Regional, or Geographic grouping of organizations).")
-          expect(charity_info.rulling_date).to eq('October, 2008')
-          expect(charity_info.deductibility).to eq("Contributions are deductible.")
-          expect(charity_info.deductibility_status).to eq('PC')
-          expect(charity_info.foundation).to eq("Organization which receives a substantial part of its support from a governmental unit or the general public   170(b)(1)(A)(vi)")
-          expect(charity_info.organization).to eq("Corporation")
-          expect(charity_info.exempt_status).to eq("Unconditional Exemption")
-          expect(charity_info.tax_period).to eq("December, 2014")
-          expect(charity_info.asset_code_desc).to eq("500,000 to 999,999")
-          expect(charity_info.income_code_desc).to eq("10,000 to 24,999")
-          expect(charity_info.filing_requirement_code_desc).to eq("Not required to file (income less than $25,000)")
-          expect(charity_info.pf_filing_requirement).to eq("No PF return")
-          expect(charity_info.accounting_period).to eq("December")
-          expect(charity_info.asset_amount).to eq("570877.00")
-          expect(charity_info.income_amount).to eq("10874.00")
-          expect(charity_info.form990).to eq("10874.00")
-          expect(charity_info.ntee_cd).to eq('A52')
-          expect(charity_info.ntee_class).to eq("Children's Museums")
-          expect(charity_info.ntee_type).to eq("Arts, Culture and Humanities")
-          expect(charity_info.accepting_donation).to eq(true)
-        end
-
       end
     end
 
