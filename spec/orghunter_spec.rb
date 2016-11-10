8248968a4e0f04191af9669bd5ddaac18b400341
Dan Dobrick <dandobrick@gmail.com>
2016-11-10 10:20:18 -0600

    Add most (not all) properties of basic_info to orghunter spec

--- spec/orghunter_spec.rb
+++ spec/orghunter_spec.rb
@@ -4,7 +4,7 @@ describe Orghunter do
   Orghunter.configure do |config|
     config.api_key = ENV['ORGHUNTER_API_KEY']
   end
-  
+
   it "has a version number" do
     expect(Orghunter::VERSION).not_to be nil
   end
@@ -24,24 +24,58 @@ describe Orghunter do
       end
   end
 
-  describe 'When making a valid query' do
-    VCR.use_cassette('basic_info_with_results') do
-      charity_info = Orghunter.basic_info('261688229')
+  describe 'Orghunter.basic_info' do
+    describe 'When making a valid query' do
+      VCR.use_cassette('basic_info_with_results') do
+        charity_info = Orghunter.basic_info('261688229')
 
-      it "returns a single charity" do
-        expect(charity_info).to be_kind_of Orghunter::Charity
+        it "returns a single charity" do
+          expect(charity_info).to be_kind_of Orghunter::Charity
+        end
+
+        it 'returns the charity with the correct ein' do
+          expect(charity_info.ein).to eq('261688229')
+        end
+
+        it 'returns the charity with the correct name' do
+          expect(charity_info.name).to eq("KIDS COWS AND MORE INC")
+        end
+
+        it 'returns more information than the basic results' do
+          expect(charity_info.in_care_of).to eq('MARK HALL')
+          expect(charity_info.street).to eq('415 W 30TH ST')
+          expect(charity_info.country).to eq('USA')
+          expect(charity_info.subsection).to eq('501(c)(3)')
+          expect(charity_info.classification).to eq("Charitable Organization")
+          expect(charity_info.affiliation).to eq("Independent - This code is used if the organization is an independent organization or an independent auxiliary (i.e., not affiliated with a National, Regional, or Geographic grouping of organizations).")
+          expect(charity_info.rulling_date).to eq('October, 2008')
+          expect(charity_info.deductibility).to eq("Contributions are deductible.")
+          expect(charity_info.deductibility_status).to eq('PC')
+          expect(charity_info.foundation).to eq("Organization which receives a substantial part of its support from a governmental unit or the general public   170(b)(1)(A)(vi)")
+          expect(charity_info.organization).to eq("Corporation")
+          expect(charity_info.exempt_status).to eq("Unconditional Exemption")
+          expect(charity_info.tax_period).to eq("December, 2014")
+          expect(charity_info.asset_code_desc).to eq("500,000 to 999,999")
+          expect(charity_info.income_code_desc).to eq("10,000 to 24,999")
+          expect(charity_info.filing_requirement_code_desc).to eq("Not required to file (income less than $25,000)")
+          expect(charity_info.pf_filing_requirement).to eq("No PF return")
+          expect(charity_info.accounting_period).to eq("December")
+          expect(charity_info.asset_amount).to eq("570877.00")
+          expect(charity_info.income_amount).to eq("10874.00")
+          expect(charity_info.form990).to eq("10874.00")
+          expect(charity_info.ntee_cd).to eq('A52')
+          expect(charity_info.ntee_class).to eq("Children's Museums")
+          expect(charity_info.ntee_type).to eq("Arts, Culture and Humanities")
+          expect(charity_info.accepting_donation).to eq(true)
+        end
+
       end
-
-      it 'returns the charity with the correct ein' do
-        expect(charity_info.ein).to eq('261688229')
-      end
-
     end
-  end
 
-  describe "When querying for something with no results" do
-    VCR.use_cassette('basic_info_no_results') do
-      xit 'returns something' do
+    describe "When querying for something with no results" do
+      VCR.use_cassette('basic_info_no_results') do
+        xit 'returns something' do
+        end
       end
     end
   end
